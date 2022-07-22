ea158e765345e77164bb11afe649b7afd6c53f9c
Vadim <vadya.dmitrichev26@gmail.com>
2022-07-22 18:36:16 +0300

    Создание ActiveRecord models и queries

---
+++ common/models/Products.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%Products}}".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property string|null $image
+ * @property float $price
+ * @property int $status
+ * @property int|null $create_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ * @property int|null $updated_by
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%Products}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'price', 'status'], 'required'],
+            [['description'], 'string'],
+            [['price'], 'number'],
+            [['status', 'create_at', 'updated_at', 'created_by', 'updated_by'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['image'], 'string', 'max' => 2000],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['updated_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'image' => 'Image',
+            'price' => 'Price',
+            'status' => 'Status',
+            'create_at' => 'Create At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\ProductQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\ProductQuery(get_called_class());
+    }
+}

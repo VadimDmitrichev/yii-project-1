7400e60c3e5fba5faff3d46804a2fd89d4da6eb8
Vadim <vadya.dmitrichev26@gmail.com>
2022-07-28 10:54:25 +0300

    Создал URl для изображения продукта через frontend и backend

--- common/config/params.php
+++ common/config/params.php
@@ -6,4 +6,7 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
+	'frontendUrl' => 'http://yii-project-1.localhost',
+	'backendUrl' => 'http://yii-project-1.localhost'
+
 ];

3eb66079c6283a36ab8ee5c6888f6ff0786a6608
Vadim <vadya.dmitrichev26@gmail.com>
2022-07-28 10:42:00 +0300

    Добавил TimestampBehavior и BlameableBehavior в Product model

--- common/models/Product.php
+++ common/models/Product.php
@@ -3,6 +3,10 @@
 namespace common\models;
 
 use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\helpers\FileHelper;
+use yii\web\UploadedFile;
 
 /**
  * This is the model class for table "{{%Product}}".
@@ -13,7 +17,7 @@ use Yii;
  * @property string|null $image
  * @property float $price
  * @property int $status
- * @property int|null $create_at
+ * @property int|null $created_at
  * @property int|null $updated_at
  * @property int|null $created_by
  * @property int|null $updated_by
@@ -28,22 +32,31 @@ class Product extends \yii\db\ActiveRecord
         return '{{%products}}';
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['name', 'price', 'status'], 'required'],
-            [['description'], 'string'],
-            [['price'], 'number'],
-            [['status', 'create_at', 'updated_at', 'created_by', 'updated_by'], 'integer'],
-            [['name'], 'string', 'max' => 255],
-            [['image'], 'string', 'max' => 2000],
-            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
-            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['updated_by' => 'id']],
-        ];
-    }
+	public function behaviors()
+	{
+		return [
+			TimestampBehavior::class,
+			BlameableBehavior::class
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules()
+	{
+		return [
+			[['name', 'price', 'status',], 'required'],
+			[['description'], 'string'],
+			[['price'], 'number'],
+			[['imageFile'], 'image', 'extensions' => 'png, jpg, jpeg, webp', 'maxSize' => 10 * 1024 * 1024],
+			[['status', 'created_at', 'updated_at', 'created_by', 'updated_by'], 'integer'],
+			[['name'], 'string', 'max' => 255],
+			[['image'], 'string', 'max' => 2000],
+			[['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+			[['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['updated_by' => 'id']],
+		];
+	}
 
     /**
      * {@inheritdoc}
